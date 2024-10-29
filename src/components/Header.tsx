757f00c8f59f52aa2a15c13d779f509e2d834823
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-29 10:55:25 -0300

    menu nav criado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,6 +11,19 @@ export const Header = () => {
                         <span className="text-blue-600 text-[27px] font-bold">X</span>
                     </Link>
                 </div>
+                <nav className="py-[10px]">
+                    <ul className="flex items-center h-[40px] gap-[40px] font-bold">
+                        <li className="">
+                            <Link href={""} className="text-[14px]">Login</Link>
+                        </li>
+                        <li>
+                            <Link href={""} className="text-[14px]">Cadastrar</Link>
+                        </li>
+                        <li>
+                            <Link href={""} className="text-[14px] bg-[#ff8100] text-white py-[5px] px-[10px] rounded hover:bg-[#e57706]">Poste um anúncio</Link>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </header>
     );
