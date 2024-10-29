3502474744b85fd5980200eb1db9181d12aa5838
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-29 10:44:31 -0300

    Criando header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export const Header = () => {
     return (
-        <header>
-            opa
+        <header className="bg-white h-[60px] border-b-[1px] border-[#ccc]">
+            <div className="max-w-[1000px] mx-auto flex">
+                <div className="flex-1 flex items-center h-[60px]">
+                    <Link href={"/"}>
+                        <span className="text-red-600 text-[27px] font-bold">O</span>
+                        <span className="text-green-600 text-[27px] font-bold">L</span>
+                        <span className="text-blue-600 text-[27px] font-bold">X</span>
+                    </Link>
+                </div>
+            </div>
         </header>
     );
 }
