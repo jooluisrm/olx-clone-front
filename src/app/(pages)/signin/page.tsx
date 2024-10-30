1027257fd978598db72a92b072b0c1961b8bb9af
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-30 10:22:39 -0300

    Login (2 - estrutura)

--- src/app/(pages)/signin/page.tsx
+++ src/app/(pages)/signin/page.tsx
@@ -1,33 +1,76 @@
+"use client"
+
+import { FormEvent, useState } from "react";
+import useApi from "@/helpers/OlxAPI";
+import { doLogin } from "@/helpers/AuthHandler";
+
 const Page = () => {
+    const api = useApi();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [rememberPassword, setRememberPassword] = useState(false);
+    const [disable, setDisabled] = useState(false);
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        setDisabled(true);
+
+        const json = await api.login(email, password);
+
+        if(json.error) {
+            setError(json.error);
+        } else {
+            doLogin(json.token, rememberPassword);
+            window.location.href = '/';
+        }
+    }
+
     return (
         <div className="max-w-[1000px] m-auto">
 
             <h1 className="text-[27px] font-bold">Login</h1>
 
             <div>
-                <form action="" className="bg-white rounded p-[10px] shadow-md">
+                <form onSubmit={handleSubmit} className="bg-white rounded p-[10px] shadow-md">
                     <label htmlFor="email" className="flex items-center p-[10px] max-w-[500px]">
                         <div className="w-[200px] text-right pr-5 font-bold text-[14px]">E-mail</div>
                         <div className="flex-[1]">
-                            <input type="email" name="" id="" className="w-full text-[14px] p-[5px] border rounded outline-none focus:border-[#333] focus:text-[#333] transition-all"/>
+                            <input
+                                disabled={disable}
+                                type="email"
+                                id="email"
+                                className="w-full text-[14px] p-[5px] border rounded outline-none focus:border-[#333] focus:text-[#333] transition-all"
+                            />
                         </div>
                     </label>
-                    <label htmlFor="email" className="flex items-center p-[10px] max-w-[500px]">
+                    <label htmlFor="senha" className="flex items-center p-[10px] max-w-[500px]">
                         <div className="w-[200px] text-right pr-5 font-bold text-[14px]">Senha</div>
                         <div className="flex-[1]">
-                            <input type="password" name="" id="" className="w-full text-[14px] p-[5px] border rounded outline-none focus:border-[#333] focus:text-[#333] transition-all"/>
+                            <input
+                                disabled={disable}
+                                type="password"
+                                id="senha"
+                                className="w-full text-[14px] p-[5px] border rounded outline-none focus:border-[#333] focus:text-[#333] transition-all"
+                            />
+                        </div>
+                    </label>
+                    <label htmlFor="lembrarsenha" className="flex items-center p-[10px] max-w-[500px]">
+                        <div className="w-[200px] text-right pr-5 font-bold text-[14px]">Lembrar senha</div>
+                        <div className="flex-[1]">
+                            <input
+                                disabled={disable}
+                                type="checkbox"
+                                id="lembrarsenha"
+                                className="text-[14px] p-[5px] border rounded outline-none focus:border-[#333] focus:text-[#333] transition-all"
+                            />
                         </div>
                     </label>
                     <label htmlFor="" className="flex items-center p-[10px] max-w-[500px]">
-                        <div className="w-[200px] text-right pr-5 font-bold text-[14px]">Lembrar senha</div>
-                        <div className="flex-[1]">
-                            <input type="checkbox" name="" id="" className="text-[14px] p-[5px] border rounded outline-none focus:border-[#333] focus:text-[#333] transition-all"/>
-                        </div>
-                    </label>
-                    <label htmlFor="email" className="flex items-center p-[10px] max-w-[500px]">
                         <div className="w-[200px] text-right pr-5 font-bold text-[14px]"></div>
                         <div className="flex-[1]">
-                            <button className="bg-[#0089ff] border-0 outline-none py-[5px] px-[10px] rounded text-white text-[15px] cursor-pointer hover:bg-[#006fce] transition-all">Fazer login</button>
+                            <button disabled={disable} className="bg-[#0089ff] border-0 outline-none py-[5px] px-[10px] rounded text-white text-[15px] cursor-pointer hover:bg-[#006fce] transition-all">Fazer login</button>
                         </div>
                     </label>
                 </form>
---
+++ src/helpers/OlxAPI.tsx
@@ -0,0 +1,8 @@
+const OlxAPI: any = {
+    login: async (email: string, password: string) => {
+
+        return {};
+    }
+};
+
+export default () => OlxAPI;
